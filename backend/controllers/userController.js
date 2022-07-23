1be749ea628d333af398fdc34f690ce48258907b
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2022-07-23 13:05:01 -0700

    Update Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,26 @@
+const asyncHandler = require("express-async-handler");
 //@desc Register a new user
 //@route /api/users
 //@access Public
 
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const { application } = require("express");
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.send("Register Route");
+});
 //@desc Login a user
-//@route /api/users/lohin
+//@route /api/users/login
 //@access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
-};
+});
 
 module.exports = {
   registerUser,
